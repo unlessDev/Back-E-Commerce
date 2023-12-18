6a79efd5693da78a0127ccc7d3244264ad0518e1
unlessDev <unlessdev@gmail.com>
2023-12-18 04:54:27 -0300

    Sección Pedidos

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -44,6 +44,11 @@ export function MainNav({
             label: 'Productos',
             active: pathname === `/${params.storeId}/products`,
         },
+                {
+            href:`/${params.storeId}/orders`,
+            label: 'Pedidos',
+            active: pathname === `/${params.storeId}/orders`,
+        },
         {
             href:`/${params.storeId}/settings`,
             label: 'Configuración',
