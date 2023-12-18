34490898620236003f19d6410be98a29f452c58b
unlessDev <unlessdev@gmail.com>
2023-12-18 06:05:03 -0300

    Fixed cloudinary

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -130,7 +130,7 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
                                         value={field.value ? [field.value] : []}
                                         disabled={loading}
                                         onChange={(url) => field.onChange(url)}
-                                        onRemove={() => field.onChange("")}
+                                        onRemove={() => field.onChange('')}
                                     />
                                 </FormControl>
                             <FormMessage />
